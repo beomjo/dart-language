ff3e356c0c3b65b737e08a93389d09dea4305c85
qjatjr1108 <qjatjr1108@naver.com>
2019-11-25 18:09:28 +0900

    Stream 예제생성 2

---
+++ src/stream/samples/StreamExample4.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+main() {
+  map();
+
+
+  transformer();
+}
+
+void transformer() {
+  //Stream sink는 스트림 이벤트를 받아들이는 것이다.
+  var transformer = StreamTransformer.fromHandlers(handleData: (value, sink) {
+    sink.add("First : $value");
+    sink.add("Second : $value");
+  });
+  
+  var stream = Stream.fromIterable(["Good", 1, 2, 3, 4, 5]);
+  stream.transform(transformer).listen((value) => print("listen : $value"));
+}
+
+void map() {
+  var streamMap = Stream.periodic(Duration(milliseconds: 200), (x) => x)
+      .take(3)
+      .map((x) => x + 10);
+
+  streamMap.listen(print);
+}
